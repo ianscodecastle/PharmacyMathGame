4c0c737373854e9b6b96468551ea6ba1656ff419
Ian Robinson <ianrobinson171@gmail.com>
2021-09-29 09:48:29 -0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: unnecessary_this, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ians App',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hello World!')),
+      body: TextInputWidget(),
+    );
+  }
+}
+
+class TestWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello World!');
+  }
+}
+
+class TextInputWidget extends StatefulWidget {
+  const TextInputWidget({Key? key}) : super(key: key);
+
+  @override
+  _TextInputWidgetState createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  final controller = TextEditingController();
+  String text = "";
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void changeText(text) {
+    if (text == "Hello World!") {
+      controller.clear();
+      text = "Hey :)";
+    }
+    setState(() {
+      this.text = text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      TextField(
+        controller: this.controller,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.computer_rounded), labelText: "Type here:"),
+        onChanged: (text) => this.changeText(text),
+      ),
+      Text(this.text)
+    ]);
+  }
+}
