a3c7982d913c0abf72860f79ff88c8679d7e3302
marystirling <marystirlingbrown@gmail.com>
2021-10-05 16:18:45 -0500

    changed text color for login

--- math_game/lib/screens/login_screen.dart
+++ math_game/lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:math_game/constants.dart';
 import 'package:flutter_svg/svg.dart';
@@ -14,13 +15,12 @@ List arr = List.filled(5, null, growable: false);
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
- 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimaryColor,
       appBar: AppBar(
-        title: const Text('Hello'),
+        title: const Text('Pharmacy Math Game'),
         backgroundColor: kPrimaryColor,
       ),
       body: Column(
@@ -44,17 +44,41 @@ class LoginScreen extends StatelessWidget {
 
           TextField(
             controller: usernameController,
+            style: TextStyle(color: Colors.white),
             decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
+
+                //fillColor: Colors.grey,
+                //filled: true,
+                //border: UnderlineInputBorder(),
+
                 labelText: 'Enter your username'),
             onChanged: (String text) => checkUsername(text),
           ),
           TextField(
             controller: passwordController,
+            style: TextStyle(color: Colors.white),
             decoration: const InputDecoration(
                 border: UnderlineInputBorder(),
                 labelText: 'Enter your password'),
             onChanged: (String text) => checkPassword(text),
+          ),
+          SizedBox(height: 15),
+
+          ElevatedButton(
+            child: Text('Forgot password'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.white,
+              onPrimary: Colors.indigo,
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              textStyle: TextStyle(
+                //color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () => {
+              forgotPassword()
+              //respond to button press
+            },
           ),
           SizedBox(height: 15),
           ElevatedButton(
@@ -93,4 +117,8 @@ void checkLogin() {
   } else {
     print("incorrect login");
   }
+}
+
+void forgotPassword() {
+  print("TODO: create forget password functionalities");
 }
