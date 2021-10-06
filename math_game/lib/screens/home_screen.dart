793c90174cf41bf45921911d197cdee701e04847
ianscodecastle <ianrobinson171@gmail.com>
2021-10-06 18:37:54 -0500

    home page widgets

--- math_game/lib/screens/home_screen.dart
+++ math_game/lib/screens/home_screen.dart
@@ -14,41 +14,66 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Hello'),
         backgroundColor: kPrimaryColor,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        //crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              'Welcome to AppName',
-              //textAlign: TextAlign.center,
-              style: TextStyle(
-                color: kSecondaryColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 40,
-                //fontFamily: 'Impact',
-              ),
-            ),
-          ),
-          SizedBox(height: 25), //adds empty space
-          ElevatedButton(
-            child: Text('BUTTON'),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.white,
-              onPrimary: Colors.indigo,
-              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-              textStyle: TextStyle(
-                //color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            onPressed: () {
-              //respond to button press
-            },
-          )
-        ],
+      body: Container(
+        decoration: BoxDecoration(gradient: kPrimaryGradient),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            AlignedMainText(),
+            SizedBox(height: 25), //adds empty space
+            StartButton()
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class AlignedMainText extends StatelessWidget {
+  const AlignedMainText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Text(
+        'Welcome to AppName',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: kSecondaryColor,
+          fontWeight: FontWeight.bold,
+          fontSize: 40,
+          //fontFamily: 'Impact',
+        ),
+      ),
+    );
+  }
+}
+
+class StartButton extends StatelessWidget {
+  const StartButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text('BUTTON'),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.white,
+        onPrimary: Colors.indigo,
+        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+        textStyle: TextStyle(
+          //color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onPressed: () {
+        //respond to button press
+      },
     );
   }
 }
